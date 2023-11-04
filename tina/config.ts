de9db9afd554c6abd12e19e48175735d96debc2b
codexuz <142141576+codexuz@users.noreply.github.com>
2023-11-04 18:12:58 +0000

    "main"

---
+++ tina/config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = "main";
+
+export default defineConfig({
+  branch,
+  clientId: '67d9fa8f-84e1-436d-8b37-5f89fc27f281', // Get this from tina.io
+  token: 'cde36b03fee84407ffbe43a890bdd3570099084f', // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/pages/blog",
+        format: 'md',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
