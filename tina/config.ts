503b5846dfad476ade04535d69bbb34f24d69274
codexuz <142141576+codexuz@users.noreply.github.com>
2023-11-04 18:29:03 +0000

    "main"

--- tina/config.ts
+++ tina/config.ts
@@ -45,21 +45,6 @@ export default defineConfig({
         label: 'Full Mock',
         name: 'fullmock',
         path: 'src/pages/full_mock',
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
       },
     ],
   },
