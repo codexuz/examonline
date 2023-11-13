dcbcb590d1175d6a9f767cf8154b0cf12eaaa7cf
codexuz <142141576+codexuz@users.noreply.github.com>
2023-11-13 09:45:22 +0500

    Add files via upload

--- src/pages/api/check.ts
+++ src/pages/api/check.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from 'astro'
+const Api = import.meta.env.Api
 import OpenAI from 'openai';
-const Api = import.meta.env.Api;
 const openai = new OpenAI({
-  apiKey: Api, 
+  apiKey: Api, // defaults to process.env["OPENAI_API_KEY"]
 });
 
 export const POST: APIRoute = async ({request }) => {
@@ -11,7 +11,7 @@ export const POST: APIRoute = async ({request }) => {
     try{
     const completion = await openai.completions.create({
     model: "gpt-3.5-turbo-instruct",
-    prompt: `You know everything about scoring IELTS essays. You assess the given essay of the given question in terms of IELTS Writing Criteria and essay words count, and provide feedback and show grammar and spelling errors.Your output must  follow this structure: “Band”: the band, "Word Count":word count, “Feedback”: your feedback, "Task achievement": task achievement, "Coherence and Cohesion": coherence and cohesiion, "Grammar": grammar, "Lexical Resource": lexical resources.
+    prompt: `You know everything about scoring IELTS essays. You assess the given essay of the given question in terms of IELTS Writing Criteria and essay words count, sentences count and paragraphs count, and list the used academic words, and provide feedback and show grammar and spelling errors.Your output must  follow this structure: "Band": the band, "Word Count":word count,"Sentences":sentences, "Paragraphs":paragraphs, “Feedback”: your feedback, "Taskachievement": task achievement, "Coherence&Cohesion": coherence and cohesion, "Grammar": grammar, "Lexical Resource": lexical resources,"Academic Words":academic words
     Question: ${body.question}
     Essay: ${body.essay}`,
     max_tokens: 800,
