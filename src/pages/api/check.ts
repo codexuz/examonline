05fe77a8425202eabc93091605d15f185c5cd7c2
codexuz <142141576+codexuz@users.noreply.github.com>
2023-12-12 23:55:41 +0500

    Update check.ts

--- src/pages/api/check.ts
+++ src/pages/api/check.ts
@@ -11,7 +11,20 @@ export const POST: APIRoute = async ({request }) => {
     try{
     const completion = await openai.completions.create({
     model: "gpt-3.5-turbo-instruct",
-    prompt: `You know everything about scoring IELTS essays. You assess the given essay of the given question in terms of IELTS Writing Criteria and essay words count, sentences count and paragraphs count, and list the used academic words, and provide feedback and show grammar and spelling errors.Your output must  follow this structure: "Band": the band, "Word Count":word count,"Sentences":sentences, "Paragraphs":paragraphs, “Feedback”: your feedback, "Taskachievement": task achievement, "Coherence&Cohesion": coherence and cohesion, "Grammar": grammar, "Lexical Resource": lexical resources,"Academic Words":academic words
+    prompt: `You know everything about scoring IELTS essays. You assess the given essay of the given question in terms of IELTS Writing Criteria and essay words count, sentences count and paragraphs count, and list the used academic words, and provide feedback and show grammar and spelling errors and provide high level essay sample to the following essay's topic.Your output must  follow this structure with html tags: 
+    <b>Band:</b> band <br>;
+    <b>WordCount:</b> wordcount <br>;
+    <b>Sentences:</b> sentences <br>;
+    <b>Paragraphs:</b> paragraphs <br>;
+    <b>Feedback</b>: feedback<br>;
+    <b>Taskachievement</b>:  task achievement<br>;
+    <b>Coherence&Cohesion:</b> coherence and cohesion<br>;
+    <b>GrammarRange:</b> grammar range <br>;
+    <b>LexicalResource:</b> lexical resources <br>;
+    <b>Academic Word:</b>academic words<br>
+    <hr>
+    <b>Sample Essay </b><br> <i>sample essay</i><br>;
+    <hr>
     Question: ${body.question}
     Essay: ${body.essay}`,
     max_tokens: 1200,
