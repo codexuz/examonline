83cbb8d4ef6d8214d527c0189887c77b3787b8c3
codexuz <142141576+codexuz@users.noreply.github.com>
2023-12-13 23:59:51 +0500

    Update check.ts

--- src/pages/api/check.ts
+++ src/pages/api/check.ts
@@ -11,18 +11,18 @@ export const POST: APIRoute = async ({request }) => {
     try{
     const completion = await openai.completions.create({
     model: "gpt-3.5-turbo-instruct",
-    prompt: `You know everything about scoring IELTS essays. You assess the given essay  ${body.essay} of the given question ${body.question} in terms of IELTS Writing Criteria and essay words count, count the exact number of words in the essay and paragraphs count, and list the used academic words, and provide feedback and show grammar and spelling errors and provide high level essay sample to the essay's question.Your output must  follow this structure with html tags and show the grammar and spelling errors with <mark> tag: 
-    <b>Band:</b> band <br>;
-    <b>Sentences:</b> sentences <br>;
-    <b>Paragraphs:</b> paragraphs <br>;
-    <b>Feedback</b>: feedback<br>;
-    <b>Task Achievement</b>:  task achievement<br>;
-    <b>Coherence and Cohesion:</b> coherence and cohesion<br>;
-    <b>Grammar Range:</b> grammar range <br>;
-    <b>Lexical Resource:</b> lexical resources <br>;
-    <b>Academic Word:</b>academic words<br>
-    <hr>
-    <b>Sample Essay </b><br> <i>sample essay</i><br>;
+    prompt: `You know everything about scoring IELTS essays. You assess the given essay  ${body.essay} of the given question ${body.question} in terms of IELTS Writing Criteria and essay sentences count, paragraphs count, and list the used academic words, and provide feedback and show grammar and spelling errors.You must follow this JSON output : 
+      {
+        "Band": "band",
+        "Sentences": "sentences",
+        "Paragraphs": "paragraphs",
+        "Feedback": "feedback",
+        "TaskAchievement": "taskachievement",
+        "CoherenceCohesion": "coherence&cohesion",
+        "GrammarRange": "grammarRange",
+        "LexicalResource": "lexicalResources",
+        "AcademicWord"={["academicWords"]},
+      }
     `,
     max_tokens: 1200,
     temperature: 0,
@@ -30,7 +30,7 @@ export const POST: APIRoute = async ({request }) => {
 
     // Return the OpenAI API response
     return new Response (
-      JSON.stringify(completion),
+      JSON.stringify(completion.choices[0].text),
       { status: 200 }
     )
 
