ff811b44999fd46b2d4c5bce8986f396e8ecc2b8
codexuz <142141576+codexuz@users.noreply.github.com>
2023-12-24 11:18:20 +0000

    "main"

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -2,14 +2,12 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPcm6WOfOOwzKBRBqNxgB71ea2TduQ-Qg",
-  authDomain: "exam-online-384406.firebaseapp.com",
-  databaseURL: "https://exam-online-384406-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "exam-online-384406",
-  storageBucket: "exam-online-384406.appspot.com",
-  messagingSenderId: "685614981927",
-  appId: "1:685614981927:web:3cba11674fdc0391723f02",
-  measurementId: "G-B2P1J1FSK7"
-  };
+  apiKey: "AIzaSyCK5iuJwf3sXSY1dUvTxgIK-5jkQh6IpEE",
+  authDomain: "exam-database-2eb01.firebaseapp.com",
+  projectId: "exam-database-2eb01",
+  storageBucket: "exam-database-2eb01.appspot.com",
+  messagingSenderId: "813791000692",
+  appId: "1:813791000692:web:bd52fd7604f6d8a62ff921"
+};
 
-  export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -3,17 +3,15 @@ import { initializeApp, cert } from "firebase-admin/app";
 
 const serviceAccount = {
   type: "service_account",
-  project_id: "exam-online-384406",
-  private_key_id: "137f70ef9dafb51882d683ae8d343be1af9f8258",
-  private_key:"-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC9ZKlZmRsxq1Qk\nlx7Jt+pNZHhD4UoNohoUtjpEHEHwCBDCqK9rch6WieNvLKtFlFJbH7Ez10C7rp6Y\n59AfUeaaIn6A8dE4wCXfmNZWoRRV6PnvJhdQciJD8i4AqTJMMCgELdVf8rrwqP79\nfKZlPuO4+EUalHY/yoZVlESL6adM3k5MMqIfoiMwwi5WKBAOlGZT+YD5uXmmK8pH\nxtRc3fEf6nnspplu4COv9//3vluKFESfnpNbWJPFWbhdNV9BhlX9DvpCO7IniyCO\nA3RwNU8o+57+ozhRrnn3WNXsU9Kfb6fAMhjLzr8dOL30ZAEiMVhYfhfZfjJQpjU0\nvf8bUwaDAgMBAAECggEAUUKaYsYNY+9Fx/lLDXOqxRABheZo1pahqBdTwpt5MxrS\nziKAOZKaSy7YdX/t8ImmGFDtv/dwX9aZV7PGmVmTnriEHKEkirvuxBQYiszMI9w1\nOIXYZPzDdPG5CTGPUh8rXDeqIGPOD6oC1hIgdrpYDKoBRFlErzQfFSLrkjhrPtA5\nG5tp5iZCYnvwa/07fLeQVwa47k85tBjviw8PEEGWWuQVnptVX/2EClArHANrdP6G\nkVF5AiKrXx3OVnLNn0xd2cSvfGi6LZBXcqiMR1OdD6KGA1oWRdDn7NqFIPK4NTLt\nYDxpzZxkTr9z4jmI5uthEzdRo9leNYrX+xPPQWfEEQKBgQD2DhQA7DgzCRcO8slS\nMuvJIA5vEvWFdY07MjrdrYO8TXN/GkzVNDkTPutZrRl8f7T67bXIVJiaftuF7Orn\nyCZee/IigV8cT2YkvnSa7wK9s5OdE+pDl2BMB27sr5pgvVIg8VN/qfykC0ZDITRz\ndNRCSqAKxNr7eZtmAF5B5BoFmQKBgQDFDE5XCtN1qSduha/T/aA13XuUqgxgm8UC\nUyGEQHnWdlOdUyEl2/YPj1+cqrTTmhc5KciUXlgQM5v7UdhkECh8E/U8w2Gu0Huu\nt3o/WqbHgf30ztqNwrKjHCWUds0eFtrCsZxUH1C31Vx0oxWrTEreZ3LjJTKtVzN5\nEiBDCS7GewKBgHmK6Lyv8u7a3aYHrgTJnIkkKZ0ae2dqctI79LIOBRXeGGA/qJZk\njpEAMpeAlydVmhudkn8bsqoWYe/c4QiPaMyhYzFCQK4mVmluHwFtcSgt7YvZC45J\n10qXHWcTMyn4OvqE2t8kI+UQoKuGWndxzWvVmwrWxOkV3/pZ0jWwLF55AoGBAIn5\nGO2nvfM4M00XPMuiXsj4wayR+UK4L5V2IhTZlLZPejLB34cD4uJ4UMmBWkaEMdir\n4i/Jc748dO7gGzXNfpuK2f/nCwH+J2xqOqYb0oYGBo2hPbVfa/f8IHA5U1zLQfhr\npvvcRYfDVQ6yltyv+giKxQ45oKdxVLBkucRWrLL7AoGBAMlMNhC7CwYjivxAS61o\nNWX5I9floIBtddZMJ0n0kfEyXmuAAfajvGxkkxLdfKUzh27eMsFrPGSV1Z54MnUF\nlPjSS2FXUyc5LVi0JzLIxa3Zgkl8MHjFaTdSgejfHeAR1lU63PiGOPV0LQECc3mD\nqzfebAawRdxa9/3EWW6tG2aP\n-----END PRIVATE KEY-----\n",
-  client_email:
-    "firebase-adminsdk-qerc0@exam-online-384406.iam.gserviceaccount.com",
-  client_id: "112688531109095437186",
+  project_id: "exam-database-2eb01",
+  private_key_id: "1f9cf52edab1bf054508703af07f1997ba5f0b03",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDceapkvyL/2lrg\nMdYg2J09qqxqkZyQy8Y1l6bBmAtE2Q/8UscvfHK5LLnwjawKSthV9ZAf2LXUOzg6\nHg7xFXWez1aaeLjB244eV/kWbovXRb/8FAJrjpUrfaBuYZFGkFzTgPPVOi4AbhMk\ngi58L0WwFD8LJ8hBOJwdEaDwTbAbwJ3lK7/ciNxSppzoGZLi5WDCxgL2+Lw47/az\nQUspuXVBv80nzUmctCe6h/RqVcBIgfwzj//TeqjT/D4EzH1w4I5NbbsYTD/YZ3Lf\ns+fNzwElftwlIRzevYFUZcsMou+r+6oY0LJiYqcw7p6gm2fYcDGHpWqExAZMdBC6\nFBISthmzAgMBAAECggEAHXghB5Spa8kMEuHGbR8sVqTx+nqaZ/Dnfkarcj6i6aHD\n1REGxmOcKAErnKorFThvgH8xXk2xRtFgO3Bvbyz+9ZUMI9tAAsrDIm4q1qM8YNqr\nWy5yK7M5YBX2HtrpYqUC1f3ArxRhjWApLIeUCWL+LcIUXSbaSabJ7clrRBjLkcx7\n1OTm75Tf4GSAuvZLxbrJsFyYEgnPxjcxTSvJAAquoD3/gxeLG9AS7NPJx40jmTud\n+7vJn2nCPuXwbjFT/2ttjhzEVLo6lA6ijDOssOJx2C7yVUmWe0JR84NtaTFzGQzc\nfWG75/G9MD7xaXevDnsQMoN/3h6XNsLRaYJDEOF8lQKBgQDwyKLmSNl0oxJ6k2oQ\n9RR7tMduEcxS8Mj4xKvSq+VCk36Eg2QRckrRgKZ93AvxxDRFCYHaMfRhJon9HwRG\njEqh+C83/HcGOgNWjsVAqzobwCITOJou+FijnFzh1HcR62i5eJMgoLvDRXA7jaEN\nbo7p9Rc8qlLy/uMZo9TMC1WAzwKBgQDqaHtQdPsY4S0B+IVnMB/Jwnm4XqDU7IP/\nbSgafKPWKyL2A0Q/6hBgP4AKcLm2P/EO8t3zWXw3ojgBNseIJhHZfKxHv9y/Cywa\norl8vRdmdhl2Ga0RotLPvqra1SnbGQNFqVtYawRhPS+3J7H2wQD+E3QB4cD9P3Mz\nNhJ8MR9p3QKBgQCd2aqRdai5gj6XBRU/0KuA/d7QFfLxXNEj5SsPjYlav/7tOIXd\nuiO6fLCpShy6fnoV2Em8lYs0NWO5ASB4b3ht/ppWBbix6E5P7ID8CESr9KViHHVX\n3Y+ekt+BXRxCLyYUkStC9JyjQ150bEDs4VHnprl5F+ARVW/kWzciHTwMmQKBgDOp\ni2QUJDM7jZvdlilD5f2lqwYDdRb4ea8V87zCs6lEIJRv697aS3mrBSFH7uZH+Wz+\nAgTcOBBa5FUAKcHMAisY2y+FDbXdVH8aVh8z3bBrXvk0d3xnkmbO8wD/8hWj0aDM\nzqESFQkJXzbSvNpRd1KIHw07IDmwfkdPJ4vPl8jRAoGBAMDBbNbwRtKaDKrQ6Wb5\nrcM+UchwJaJTJ22HTcYC9i3hFD054CgxTGtpTD95rTMCGzMJK929ssYavQG/Jn6N\nGhC0f/HmS+cLSWEhEr5Zi2HKYzAJ+nhaWK3lHR7mgKbsyv24BRWAcuqipiuzeBYy\niBSjuGitYOX3hsDGlq0K9Mde\n-----END PRIVATE KEY-----\n",
+  client_id: "112639326834787560887",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-qerc0%40exam-online-384406.iam.gserviceaccount.com",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-73uab%40exam-database-2eb01.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
 };
 
 export const app = initializeApp({
