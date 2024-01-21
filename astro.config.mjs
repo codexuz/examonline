dea664ad62a87b1d023faf05b7df5a974d0165dc
codexuz <142141576+codexuz@users.noreply.github.com>
2024-01-21 14:48:30 +0500

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService()
-  },
   output: 'server',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), react()],
   adapter: vercel()
 });
