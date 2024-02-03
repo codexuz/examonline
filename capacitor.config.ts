af165f98f32cf15fc832bf62f7589da09b67ca9c
codexuz <142141576+codexuz@users.noreply.github.com>
2024-02-04 01:08:56 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'vercel.multiexam.app',
   appName: 'examonline edumo',
-  webDir: 'server',
+  webDir: 'vercel',
   server: {
     androidScheme: 'https'
   }
