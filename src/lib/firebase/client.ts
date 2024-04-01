2e72b7faeebe8f0b6f96b32eb8e735be7789538f
codexuz <142141576+codexuz@users.noreply.github.com>
2024-04-01 10:19:54 +0300

    Update client.ts

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK5iuJwf3sXSY1dUvTxgIK-5jkQh6IpEE",
+  apiKey: "AIzaSyDF9VuBa0ZlqlKOknj3IrdE--olEiN5DPQ",
   authDomain: "exam-database-2eb01.firebaseapp.com",
   projectId: "exam-database-2eb01",
   storageBucket: "exam-database-2eb01.appspot.com",
   messagingSenderId: "813791000692",
-  appId: "1:813791000692:web:bd52fd7604f6d8a62ff921"
+  appId: "1:813791000692:web:b423c8aeb48652832ff921"
 };
 
 export const app = initializeApp(firebaseConfig);
