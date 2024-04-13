e16f35794ec631df86f3708d83035b45d27b1f93
codexuz <142141576+codexuz@users.noreply.github.com>
2024-04-13 15:41:08 +0000

    "main"

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
-interface ImportMetaEnv {
-    readonly Api: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
