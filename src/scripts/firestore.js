b5dcc10dffc965fb46957187a0f516bb0419540d
codexuz <142141576+codexuz@users.noreply.github.com>
2024-05-17 22:40:44 +0500

    Update firestore.js

--- src/scripts/firestore.js
+++ src/scripts/firestore.js
@@ -10,3 +10,37 @@ import { db } from "@lib/firebase/client";
   status: 'unpaid'
 });
 }
+
+
+// Calculate the date one month from now
+
+
+
+export const updatePaymentInfoFifty = async(userId) => {
+const currentDate = new Date();
+const nextMonthDate = new Date(currentDate.setMonth(currentDate.getMonth() + 1));
+console.log(nextMonthDate)
+  const userRef = doc(db, "users", userId);
+ await updateDoc(userRef, {
+  expiresAt: nextMonthDate,
+  tariff: 'basic',
+  balance: '50. 000 UZS',
+  status: 'paid'
+});
+
+}
+
+
+export const updatePaymentInfoHundred = async(userId) => {
+  const currentDate = new Date();
+  const nextMonthDate = new Date(currentDate.setMonth(currentDate.getMonth() + 2));
+  console.log(nextMonthDate)
+    const userRef = doc(db, "users", userId);
+   await updateDoc(userRef, {
+    expiresAt: nextMonthDate,
+    tariff: 'pro',
+    balance: '100. 000 UZS',
+    status: 'paid'
+  });
+  
+  }
